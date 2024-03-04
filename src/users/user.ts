cbb12d6de641e76553a3b356f262f7bd838a253d
Flozoui <96843353+Flozoui@users.noreply.github.com>
2024-03-04 23:54:15 +0100

    Update user.ts

--- src/users/user.ts
+++ src/users/user.ts
@@ -7,14 +7,37 @@ export type SendMessageBody = {
   destinationUserId: number;
 };
 
+var lastReceivedMessage: string | null = null;
+var lastastSentMessage: string | null = null;
+
 export async function user(userId: number) {
   const _user = express();
   _user.use(express.json());
   _user.use(bodyParser.json());
 
-  // TODO implement the status route
-  // _user.get("/status", (req, res) => {});
+  // DONE implement the status route
+  _user.get("/status", (req, res) => {res.send("live");});
 
+  _user.get("/getLastReceivedMessage",  (req, res) => {
+    res.json({ result: lastReceivedMessage });
+  });
+
+  _user.get("/getLastSentMessage", (req, res) => {
+    res.json({ result: lastastSentMessage });
+  });
+
+  _user.post("/message", (req, res) => {
+    const sendMessageBody = req.body as SendMessageBody;
+    lastReceivedMessage = sendMessageBody.message;
+    res.send("message received");
+  });
+
+  _user.post('/sendMessage', async (req, res) => {
+    const sendMessageBody = req.body as SendMessageBody;
+
+  });
+
+  
   const server = _user.listen(BASE_USER_PORT + userId, () => {
     console.log(
       `User ${userId} is listening on port ${BASE_USER_PORT + userId}`
